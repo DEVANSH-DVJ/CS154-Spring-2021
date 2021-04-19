a88ee9518320b47c0e872bb45b1b296fc78ebb03
devansh-dvj <devansh.dvj@gmail.com>
2021-04-19 23:28:02 +0530

    Lab 4: Updated PS

--- Lab 4/PS/1window.cpp
+++ Lab 4/PS/1window.cpp
@@ -1,12 +1,15 @@
-#include <FL/Fl.H>        // needed in every fltk program
+
+#include <FL/Fl.H> // needed in every fltk program
 #include <FL/Fl_Window.H> // needed to use the Fl_Window class
 
 int main(int argc, char *argv[]) {
-  Fl_Window *window;
+    	Fl_Window *window;
 
-  window = new Fl_Window(600, 600, "DEMO"); // outer window
+window = new Fl_Window (600,600,"DEMO"); // outer window
 
-  window->end();
-  window->show();
-  return (Fl::run()); // the process waits from here on for events
+    	window->end();
+    	window->show();
+    	return(Fl::run());  // the process waits from here on for events
 }
+
+
--- Lab 4/PS/4handle.cpp
+++ Lab 4/PS/4handle.cpp
@@ -1,45 +1,48 @@
-#include <FL/Fl.H>              // needed in every fltk program
-#include <FL/Fl_Button.H>       // needed to use the Fl_Box class
-#include <FL/Fl_Light_Button.H> // needed to use the Fl_Box class
+
+#include <FL/Fl.H> // needed in every fltk program
+#include <FL/Fl_Window.H> // needed to use the Fl_Window class
+#include <FL/Fl_Button.H> // needed to use the Fl_Box class
 #include <FL/Fl_Radio_Button.H> // needed to use the Fl_Box class
-#include <FL/Fl_Window.H>       // needed to use the Fl_Window class
+#include <FL/Fl_Light_Button.H> // needed to use the Fl_Box class
+
 
 class MyButton : public Fl_Button {
-  int state;
+	int state;
+	public:
+		MyButton (int x, int y, int w, int h, const char *l); 
+		int handle(int e); // e is incoming mouse event of different kinds
+ };
 
-public:
-  MyButton(int x, int y, int w, int h, const char *l);
-  int handle(int e); // e is incoming mouse event of different kinds
-};
-
-MyButton ::MyButton(int x, int y, int w, int h, const char *l)
-    : Fl_Button(x, y, w, h, l) {
-  state = 0;
+MyButton :: MyButton (int x,int y, int w, int h, const char *l) : Fl_Button (x,y,w,h,l) {
+	state =0;
 }
-
-int MyButton ::handle(int e) {
-  if (state == 0) {
-    this->label("00");
-    state = 1;
-  } else {
-    this->label("11");
-    state = 0;
-  }
-  return 1;
+int MyButton :: handle (int e) {
+	if (state ==0) { 
+		this->label("00");
+		state = 1;
+	}
+	else {
+		this->label("11");
+		state = 0;
+	}
+	return 1;
 };
 
 int main(int argc, char *argv[]) {
-  Fl_Window *window;
+    	Fl_Window *window;
 
-  window = new Fl_Window(600, 600, "DEMO"); // outer window
+window = new Fl_Window (600,600,"DEMO"); // outer window
 
-  Fl_Button *button1 = new MyButton(100, 100, 100, 100, "Box 1!");
-  window->color(FL_WHITE);
-  button1->down_color(FL_RED);
+        Fl_Button *button1 = new MyButton (100,100,100,100,"Box 1!");
+	window->color(FL_WHITE);
+	button1->down_color(FL_RED);
 
-  button1->label("10");
+	button1->label("10");
 
-  window->end();
-  window->show();
-  return (Fl::run()); // the process waits from here on for events
+
+    	window->end();
+    	window->show();
+    	return(Fl::run());  // the process waits from here on for events
 }
+
+
