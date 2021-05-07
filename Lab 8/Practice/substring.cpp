1a8172405649fcc2f83c0b919f0d1ec7cd724a6d
devansh-dvj <devansh.dvj@gmail.com>
2021-05-07 18:34:32 +0530

    Lab 8: Finished substring

--- Lab 8/Practice/substring.cpp
+++ Lab 8/Practice/substring.cpp
@@ -1,19 +1,48 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 8 - Subtring Matching - with bugs and assertions
+ */
+
 #include <cassert>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int find(string str, string sub) {
+  int L = str.length(), l = sub.length();
 
-  // add precondition assertions
+  assert(l > 0);  // substring should have a positive length
+  assert(L >= l); // string should not be shorter than substring
 
-  // add logic
+  int i = 0;
+  for (i = 0; i < L - l; i++) {
+    bool found = true;
+    for (int j = i; j < l; j++) {
+      if (str[i + j] != sub[j]) {
+        found = false;
+        break;
+      }
+    }
+    if (found)
+      return i + 1; // index at which substring starts
+  }
 
-  // add postcondition assertions
+  assert(i == L - l); // only return -1 if all starting points checked
+
+  return -1; // No substring found
 }
 
 int main() {
 
-  // add testing code here which fails the above function and that failure
-  // gets caught by the assertions that you have
+  string str = "Computer Science and Engineering";
+
+  cout << find(str, "Engineering") << endl;
+  cout << find(str, "Science") << endl;
+  cout << find(str, "Computer") << endl;
+  cout << find(str, "B. Tech., Indian Institute of Technology, Bombay") << endl;
+  cout << find(str, "Electrical Engineering") << endl;
+  cout << find(str, "") << endl;
+
+  return 0;
 }
