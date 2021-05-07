dcc9aa17eb1a7860968c133f49c9c1a559c1a063
devansh-dvj <devansh.dvj@gmail.com>
2021-05-07 18:32:12 +0530

    Lab 8: Finished str2int

--- Lab 8/Practice/str2int.cpp
+++ Lab 8/Practice/str2int.cpp
@@ -1,23 +1,38 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 8 - String to Integer - with bugs and assertions
+ */
+
 #include <cassert>
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int str2int(string s) {
-  int v;
-  // define your other needed variables
+  int res = 0, i = 0, len = s.length();
 
-  // add assertions here first
+  assert(len <= 9); // string should be compatible to size of int
 
-  // the logic, which does not check for any condition, can be buggy
+  while (i != len) {
+    assert(i < len);                    // i should not exceed length of string
+    assert(s[i] >= '0' && s[i] <= '9'); // string characters must be a digit
+    res = 10 * res + s[i] - '0';
+    ++i;
+  }
 
-  // add assertios for postcondition
+  assert(i == len); // should go thru all characters in the string
 
-  return v;
+  return res;
 };
 
 int main() {
 
-  // develop main which catches errors through assertions
-  // report bugs
+  cout << str2int("-4") << endl;
+
+  cout << str2int("34t45") << endl;
+
+  cout << str2int("1248664830475668493") << endl;
+
+  return 0;
 }
