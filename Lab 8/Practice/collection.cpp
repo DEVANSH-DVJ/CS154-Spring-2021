d19c61acbc854dd36a70c521abdd40cbae16ba18
devansh-dvj <devansh.dvj@gmail.com>
2021-05-07 18:30:28 +0530

    Lab 8: Finished collection

--- Lab 8/Practice/collection.cpp
+++ Lab 8/Practice/collection.cpp
@@ -1,36 +1,66 @@
-#include <iostream>
-using namespace std;
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 8 - Collection - with bugs and assertions
+ */
+
 #include <cassert>
-// A program which is not correct
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @class Collection
+ * @brief It has bugs and assertions catch it
+ */
 class Collection {
   int *S;
   int cap, card;
 
 public:
   Collection(int sz) {
+    assert(sz >= 0); // max size of collection should be non-negative
+
     S = new int[sz];
     card = 0;
-    //... add assertion(s)...
+
+    assert(card == 0); // collection should be empty at start
+    assert(cap == sz); // cap should be equal to max size of collection
   }
 
   void push_back(int elem) {
-    //... add assertion(s)...
+    assert(card < cap); // collection should not be full
 
-    //... add logic
+    S[card] = elem;
+    cout << elem << " added in Collection\n";
+    card++;
 
-    //... add assertion(s)...
+    assert(card >= 0);   // cardinality should not negative
+    assert(card <= cap); // cardinality should not have exceed full
   }
 
   int pop_back() {
-    //... add assertion(s)...
+    assert(card > 0); // collection should not be empty
 
-    //... add logic
+    card--;
+    cout << S[card] << " popped out of Collection\n";
 
-    //... add assertion(s)...
+    assert(card >= 0);   // cardinality should not negative
+    assert(card <= cap); // cardinality should not have exceed full
+
+    return S[card];
   }
 };
+
 int main() {
 
-  // add code that tests the class above and fails it, which gets caught by
-  // assertions
+  Collection c1(-1);
+  Collection c2(4);
+
+  for (int i = 0; i < 5; i++)
+    c2.push_back(i);
+
+  for (int i = 0; i < 6; i++)
+    c2.pop_back();
+
+  return 0;
 }
