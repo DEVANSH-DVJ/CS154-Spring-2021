715350afd52822fda241fd9e10ed0950e3e6855a
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 19:04:43 +0530

    Lab 9: Finished trythis1

--- Lab 9/Practice/trythis1.cpp
+++ Lab 9/Practice/trythis1.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
-
 using namespace std;
 
 void f(int x, int y) { cout << x << " " << y << endl; }
 
 int main() {
+
   int i = 10;
 
-  f(i, i++);
-
-  // i = 100;
+  f(i++, ++i);
+  cout << i << endl;
 
   f(++i, i++);
-
   cout << i << endl;
+
+  f(i, i++);
+  cout << i << endl;
+
+  f(i, ++i);
+  cout << i << endl;
+
+  return 0;
 }
