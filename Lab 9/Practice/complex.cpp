eb451f6470dccc873dfccae9d335b9f830d9c023
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 18:33:29 +0530

    Lab 9: Finished complex

--- Lab 9/Practice/complex.cpp
+++ Lab 9/Practice/complex.cpp
@@ -12,28 +12,84 @@ class Complex {
   float i, j;
 
 public:
-  Complex(int x, int y) {
+  Complex() : Complex(0, 0) {}
+
+  Complex(float x, float y) {
     i = x;
     j = y;
   }
 
   Complex operator+(Complex c) {
-    cout << i << "," << j << "+" << c.i << "," << c.j << endl;
+    cout << "(" << i << " + i" << j << ")"
+         << " + "
+         << "(" << c.i << " + i" << c.j << ")"
+         << " = "
+         << "(" << i + c.i << " + i" << j + c.j << ")" << endl;
     return Complex(i + c.i, j + c.j);
   }
 
-  void prn() { cout << i << "," << j << endl; }
+  Complex operator-(Complex c) {
+    cout << "(" << i << " + i" << j << ")"
+         << " - "
+         << "(" << c.i << " + i" << c.j << ")"
+         << " = "
+         << "(" << i - c.i << " + i" << j - c.j << ")" << endl;
+    return Complex(i - c.i, j - c.j);
+  }
+
+  Complex operator*(Complex c) {
+    cout << "(" << i << " + i" << j << ")"
+         << " * "
+         << "(" << c.i << " + i" << c.j << ")"
+         << " = "
+         << "(" << i * c.i - j * c.j << " + i" << i * c.j + j * c.i << ")"
+         << endl;
+    return Complex(i * c.i - j * c.j, i * c.j + j * c.i);
+  }
+
+  Complex operator/(Complex c) {
+    // Check division by zero
+    if (c.i == 0 && c.j == 0) {
+      cout << "Can't divide by 0" << endl;
+      return Complex();
+    }
+
+    float mod = c.i * c.i + c.j * c.j;
+    cout << "(" << i << " + i" << j << ")"
+         << " / "
+         << "(" << c.i << " + i" << c.j << ")"
+         << " = "
+         << "(" << (i * c.i + j * c.j) / mod << " + i"
+         << (j * c.i - i * c.j) / mod << ")" << endl;
+    return Complex((i * c.i + j * c.j) / mod, (j * c.i - i * c.j) / mod);
+  }
+
+  void prn() { cout << "(" << i << " + i" << j << ")" << endl; }
 };
 
 int main() {
 
-  Complex c1(10, 10), c2(20, 20), c3(0, 0);
+  Complex c1(10, 10), c2(20, 20), c3;
+
+  c3 = c1 - c2;
+  cout << "c3: ";
+  c3.prn();
+
+  c3 = c1 * c2;
+  cout << "c3: ";
+  c3.prn();
+
+  c3 = c1 / c2;
+  cout << "c3: ";
+  c3.prn();
+
+  c3 = c1 / Complex(0, 0);
 
   c3 = c1 + c2;
-
+  cout << "c3: ";
   c3.prn();
 
   c3 = c3 + c1 + c2 + c2;
-
+  cout << "c3: ";
   c3.prn();
 }
