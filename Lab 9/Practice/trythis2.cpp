bbb51691bec17c387c39fdcdfe21003e03b76b86
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 19:04:55 +0530

    Lab 9: Finished trythis2

--- Lab 9/Practice/trythis2.cpp
+++ Lab 9/Practice/trythis2.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 void f(int x, int y, int z) { cout << x << " " << y << " " << z << endl; }
 
 int main() {
+
   int i = 10;
 
-  f(i, i++, ++i);
-
-  // i = 100;
+  f(i++, ++i, ++i);
+  cout << i << endl;
 
   f(++i, i++, ++i);
-
   cout << i << endl;
+
+  f(++i, ++i, ++i);
+  cout << i << endl;
+
+  return 0;
 }
