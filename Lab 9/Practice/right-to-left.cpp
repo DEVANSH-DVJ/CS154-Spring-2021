683c2bfc21f61ebb7bc018642e56243be6767811
devansh-dvj <devansh.dvj@gmail.com>
2021-05-14 18:59:14 +0530

    Lab 9: Finished right-to-left

--- Lab 9/Practice/right-to-left.cpp
+++ Lab 9/Practice/right-to-left.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
+
 class A {
   int id;
 
 public:
   A(int v) { id = v; }
   A &operator=(A &a) {
-    cout << "executing " << id << "=" << a.id << endl;
+    cout << "Executing " << id << "=" << a.id << endl;
     return *this;
   }
 };
 
 int main() {
+
   int a = 1, b = 2, c = 3;
   a = (++b) = (c++);
-  cout << a << b << c << endl;
+  cout << a << " " << b << " " << c << endl;
+
   A a1(1), a2(2), a3(3);
-  a1 = a2 = a3;
+	a2 = a3;
+	a1 = a2;
+	a3 = a1 = a3 = a2;
+
+  return 0;
 }
