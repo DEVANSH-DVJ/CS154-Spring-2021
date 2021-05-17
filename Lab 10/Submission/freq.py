6c6ad66952a9f09c76982542e69074f9373f9ad9
devansh-dvj <devansh.dvj@gmail.com>
2021-05-17 20:21:15 +0530

    Lab 10: Updated freq.py - using dicts

--- Lab 10/Submission/freq.py
+++ Lab 10/Submission/freq.py
@@ -1,6 +1,16 @@
 import sys
 
 
+def count(x, dic):
+    for w in x:
+        dic[w] = dic.get(w, 0) + 1
+    return dic
+
+
 print('Method 1: using sets')
 print1 = lambda x: [print(e, x.count(e)) for e in set(x)]
 print1(open(sys.argv[1], 'r').read().split())
+
+print('\nMethod 2: using dicts')
+print2 = lambda x: [print(k, v) for k, v in x.items()]
+print2(count(open(sys.argv[1], 'r').read().split(), {}))
