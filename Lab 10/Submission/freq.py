1840511c7782c758891e7b9b49bd0ae1832b099a
devansh-dvj <devansh.dvj@gmail.com>
2021-05-17 21:52:13 +0530

    Lab 10: Updated freq.py - added comments

--- Lab 10/Submission/freq.py
+++ Lab 10/Submission/freq.py
@@ -1,16 +1,19 @@
-import sys
+# Author: Devansh Jain (190100044)
+# Lab 10 - Word Frequency
+
+import sys  # For command line arguments
 
 
-def count(x, dic):
-    for w in x:
+def count(arr, dic):
+    for w in arr:
         dic[w] = dic.get(w, 0) + 1
     return dic
 
 
 print('Method 1: using sets')
-print1 = lambda x: [print(e, x.count(e)) for e in set(x)]
+print1 = lambda arr: [print(e, arr.count(e)) for e in set(arr)]
 print1(open(sys.argv[1], 'r').read().split())
 
 print('\nMethod 2: using dicts')
-print2 = lambda x: [print(k, v) for k, v in x.items()]
+print2 = lambda dic: [print(k, v) for k, v in dic.items()]
 print2(count(open(sys.argv[1], 'r').read().split(), {}))
