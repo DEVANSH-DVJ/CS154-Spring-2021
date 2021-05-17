c76948a4192c14e94cc627fb01c3fc272386bad2
devansh-dvj <devansh.dvj@gmail.com>
2021-05-17 20:21:05 +0530

    Lab 10: Updated freq.py - using sets

--- Lab 10/Submission/freq.py
+++ Lab 10/Submission/freq.py
@@ -1,3 +1,6 @@
 import sys
-f = lambda x: [print(e, x.count(e)) for e in set(x)]
-f(open(sys.argv[1], 'r').read().split())
+
+
+print('Method 1: using sets')
+print1 = lambda x: [print(e, x.count(e)) for e in set(x)]
+print1(open(sys.argv[1], 'r').read().split())
