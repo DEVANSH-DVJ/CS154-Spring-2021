390fd63fef8ea44437333a31fb0486d88d5960c8
devansh-dvj <devansh.dvj@gmail.com>
2021-05-31 15:05:31 +0530

    Lab 11: 1a - Added cli input

--- Lab 11/Submission/1a.py
+++ Lab 11/Submission/1a.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def ack(m, n):
     if m == 0:
         return n + 1
@@ -6,3 +9,10 @@ def ack(m, n):
         return ack(m - 1, 1)
 
     return ack(m - 1, ack(m, n - 1))
+
+
+if __name__ == '__main__':
+    m = int(sys.argv[1])
+    n = int(sys.argv[2])
+
+    print(ack(m, n))
