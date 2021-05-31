e62732731b5a87185f384fe6323792d4f3672010
devansh-dvj <devansh.dvj@gmail.com>
2021-05-31 17:50:02 +0530

    Lab 11: 1d - Added memoization and lookups

--- Lab 11/Submission/1d.py
+++ Lab 11/Submission/1d.py
@@ -6,24 +6,41 @@ Lab 11 1D - Memoization - Counting Ackerman Function calls and table look ups
 import sys
 
 
-# Global counter for calls
+# Global counter for calls and look ups
 count = 0
+lookups = 0
 
 
-def ack(m, n):
+def ack(m, n, table):
     '''
-    Recursively compute Ackerman number for inputs m, n
+    Compute Ackerman number for inputs m, n using Dynamic Programming
     '''
-    global count
+    global count, lookups
     count = count + 1
 
     if m == 0:
-        return n + 1
+        table[(m, n)] = n + 1
+        return table[(m, n)]
 
     if n == 0:
-        return ack(m - 1, 1)
+        if (m - 1, 1) in table:
+            lookups = lookups + 1
+            table[(m, n)] = table[(m - 1, 1)]
+        else:
+            table[(m, n)] = ack(m - 1, 1, table)
+        return table[(m, n)]
 
-    return ack(m - 1, ack(m, n - 1))
+    if (m, n - 1) in table:
+        lookups = lookups + 1
+        temp = table[(m, n - 1)]
+        if (m - 1, temp) in table:
+            lookups = lookups + 1
+            table[(m, n)] = table[(m - 1, temp)]
+        else:
+            table[(m, n)] = ack(m - 1, temp, table)
+    else:
+        table[(m, n)] = ack(m - 1, ack(m, n - 1, table), table)
+    return table[(m, n)]
 
 
 if __name__ == '__main__':
@@ -45,5 +62,5 @@ if __name__ == '__main__':
         print('Both m and n have to be non-negative integers')
         exit(1)
 
-    print('ack({}, {}) = {}'.format(m, n, ack(m, n)))
+    print('ack({}, {}) = {}'.format(m, n, ack(m, n, {})))
     print('Number of function calls = {}'.format(count))
