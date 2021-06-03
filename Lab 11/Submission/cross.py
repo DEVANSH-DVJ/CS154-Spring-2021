b30fb5d212ab73866ce9c7d31662fe6ef31235a3
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 16:50:26 +0530

    Lab 11: Finished cross

--- Lab 11/Submission/cross.py
+++ Lab 11/Submission/cross.py
@@ -1,7 +1,20 @@
+'''
+Author: Devansh Jain (190100044)
+Lab 11 2 - Defining Cartesian Cross Product Function
+'''
+
+
 def cross(arr1, arr2):
+    '''
+    Cartesian Cross product of arr1 and arr2
+    '''
+
+    # if either is empty then empty cartesian cross product
     if len(arr1) == 0 or len(arr2) == 0:
         return []
 
+    # Cross product with first element of arr1 with arr2
+    # followed by cross product with rest of elements of arr1 with arr2
     arr = [(arr1[0], elem) for elem in arr2] + cross(arr1[1:], arr2)
 
     return arr
