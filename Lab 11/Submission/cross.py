7cde65fead2eb1f16ed9245330ed786bc4fb2df1
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 16:40:24 +0530

    Lab 11: cross - pretty print

--- Lab 11/Submission/cross.py
+++ Lab 11/Submission/cross.py
@@ -2,9 +2,7 @@ def cross(arr1, arr2):
     if len(arr1) == 0 or len(arr2) == 0:
         return []
 
-    arr = [(arr1[0], elem) for elem in arr2]
-
-    arr += cross(arr1[1:], arr2)
+    arr = [(arr1[0], elem) for elem in arr2] + cross(arr1[1:], arr2)
 
     return arr
 
@@ -13,4 +11,6 @@ if __name__ == '__main__':
     l1 = [1, 2, 4, 9]
     l2 = [141, '141', 42, True]
 
-    print(cross(l1, l2))
+    c = cross(l1, l2)
+    print('Cross product has {} terms'.format(len(c)))
+    print(c)
