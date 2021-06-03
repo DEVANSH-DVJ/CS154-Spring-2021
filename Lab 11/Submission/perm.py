a79f7c95d564105443dde08ea3d7c0f339dd424a
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 16:52:24 +0530

    Lab 11: Finished perm

--- Lab 11/Submission/perm.py
+++ Lab 11/Submission/perm.py
@@ -1,9 +1,24 @@
+'''
+Author: Devansh Jain (190100044)
+Lab 11 2 - Defining Permutation Function
+'''
+
+
 def perm(arr):
+    '''
+    List of all Permutations of arr
+    '''
+
+    # If empty then only one permutation - phi
     if len(arr) == 0:
         return [[]]
 
+    # Initialize result
     res = []
+
     for n in range(len(arr)):
+        # Permute the array excluding the nth element
+        # and add the element at the start of it
         value = arr.pop(n)
         res += [[value] + perm_elem for perm_elem in perm(arr)]
         arr.insert(n, value)
