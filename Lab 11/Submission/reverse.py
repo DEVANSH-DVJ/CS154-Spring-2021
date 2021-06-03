12c8a21bee1fe2744aa49ef9575b13a7099227a8
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 15:20:32 +0530

    Lab 11: Tested reverse function

--- Lab 11/Submission/reverse.py
+++ Lab 11/Submission/reverse.py
@@ -2,3 +2,11 @@ def reverse(arr):
     if len(arr) == 0:
         return []
     return reverse(arr[1:]) + [arr[0]]
+
+
+if __name__ == '__main__':
+    l1 = [1, 0, 7, 14, 9.9]
+    l2 = [1.1, 1, '123', 'hi', True]
+
+    print(reverse(l1))
+    print(reverse(l2))
