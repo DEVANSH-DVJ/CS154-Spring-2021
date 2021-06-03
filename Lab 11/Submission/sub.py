e9a214577d035426b22187c1c7b485edd313522d
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 15:31:52 +0530

    Lab 11: Tested sub function

--- Lab 11/Submission/sub.py
+++ Lab 11/Submission/sub.py
@@ -7,3 +7,14 @@ def sub(sub_arr, arr):
             return sub(sub_arr, arr[1:])
 
     return True
+
+
+if __name__ == '__main__':
+    l1 = [1, 0, 7, 14, 9.9]
+    l2 = [1.1, 1, '123', 'hi', True]
+    l3 = [1, False]
+    l4 = [1, 0]
+
+    print(sub(l2, l1))
+    print(sub(l3, l1))
+    print(sub(l4, l1))
