91ea67ab218693c2778d0885a93065f187c0551f
devansh-dvj <devansh.dvj@gmail.com>
2021-06-03 16:15:57 +0530

    Lab 11: Tested perm function

--- Lab 11/Submission/perm.py
+++ Lab 11/Submission/perm.py
@@ -9,3 +9,17 @@ def perm(arr):
         arr.insert(n, value)
 
     return res
+
+
+if __name__ == '__main__':
+    l1 = [1, 2, 'hi', True]
+    l2 = [1, 2, 3]
+
+    p1 = perm(l1)
+    p2 = perm(l2)
+
+    print('Number of permutation of {} are {}'.format(l1, len(p1)))
+    print(p1)
+
+    print('Number of permutation of {} are {}'.format(l2, len(p2)))
+    print(p2)
