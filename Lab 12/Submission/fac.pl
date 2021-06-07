4e45a736cf57f88bfd8c3c5623da9bf8dd938112
devansh-dvj <devansh.dvj@gmail.com>
2021-06-07 22:25:39 +0530

    Lab 12: Finished fac

--- Lab 12/Submission/fac.pl
+++ Lab 12/Submission/fac.pl
@@ -1,2 +1,8 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 12 - Computing factorial
+ */
+
+% Basic Usage: fac(+Number, -Factorial)
 fac(0,1).
 fac(N,NF) :- N1 is N-1, fac(N1,N1F), NF is N1F * N.
