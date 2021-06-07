769f08d7f2ca8dca989632b5fc6d94c21bb90133
devansh-dvj <devansh.dvj@gmail.com>
2021-06-07 22:25:23 +0530

    Lab 12: Finished app

--- Lab 12/Submission/app.pl
+++ Lab 12/Submission/app.pl
@@ -1,2 +1,8 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 12 - Appending a list to another list
+ */
+
+% Basic Usage: app(+List1, +List2, -CombinedList)
 app([],Y,Y).
 app([H|X],Y,[H|Z]) :- app(X,Y,Z).
