2442a4cd6b0af5614ac77336ae66ff449732dc32
devansh-dvj <devansh.dvj@gmail.com>
2021-06-07 22:25:42 +0530

    Lab 12: Finished min

--- Lab 12/Submission/min.pl
+++ Lab 12/Submission/min.pl
@@ -1,3 +1,9 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 12 - Finding minimum in a list
+ */
+
+% Basic Usage: min(+List, -MinimumElement)
 min([X],X).
 min([H,K|T],M) :- min([H|T],M), H =< K.
 min([H,K|T],M) :- min([K|T],M), H > K.
