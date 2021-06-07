e69c8ab3961b05b45fd3f548140a76805c1e79c8
devansh-dvj <devansh.dvj@gmail.com>
2021-06-07 22:25:46 +0530

    Lab 12: Finished pal

--- Lab 12/Submission/pal.pl
+++ Lab 12/Submission/pal.pl
@@ -1,3 +1,9 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 12 - Check if a string is a palindrome
+ */
+
+% Basic Usage: pal(+List)
 pal([]).
 pal([_]).
 pal(P) :- append([H|T],[H],P), pal(T).
