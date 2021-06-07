c0be1c40e903d2cceac588cdfddc73620f91654b
devansh-dvj <devansh.dvj@gmail.com>
2021-06-07 22:25:35 +0530

    Lab 12: Finished cnt

--- Lab 12/Submission/cnt.pl
+++ Lab 12/Submission/cnt.pl
@@ -1,3 +1,9 @@
+/**
+ * Author: Devansh Jain (190100044)
+ * Lab 12 - Counting occurance of an element in a list
+ */
+
+% Basic Usage: cnt(+List, +Element, -Count)
 cnt([],_,0).
 cnt([X|T],X,Y) :- cnt(T,X,Z), Y is Z+1.
 cnt([X1|T],X,Z) :- cnt(T,X,Z), X1 \= X.
