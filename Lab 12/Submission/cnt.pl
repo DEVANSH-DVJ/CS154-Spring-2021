f9de4859bd83df923fa554000e9973af013d67a7
devansh-dvj <devansh.dvj@gmail.com>
2021-06-09 17:34:07 +0530

    Lab 12: Added comments to cnt

--- Lab 12/Submission/cnt.pl
+++ Lab 12/Submission/cnt.pl
@@ -3,7 +3,13 @@
  * Lab 12 - Counting occurance of an element in a list
  */
 
-% Basic Usage: cnt(+List, +Element, -Count)
+% Usage: cnt(List, Element, Count)
+
+% For empty list, every element has count 0
 cnt([],_,0).
+% If the head of list is the desired element then
+%   count is 1 + count in tail of list
 cnt([X|T],X,Y) :- cnt(T,X,Z), Y is Z+1.
+% If the head of list is not the desired element then
+%   count is count in tail of list
 cnt([X1|T],X,Z) :- cnt(T,X,Z), X1 \= X.
