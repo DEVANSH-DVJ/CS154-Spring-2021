b2d09bae48b6065909bcb42f4edefdf571b6f6d4
devansh-dvj <devansh.dvj@gmail.com>
2021-06-09 17:34:11 +0530

    Lab 12: Added comments to fac

--- Lab 12/Submission/fac.pl
+++ Lab 12/Submission/fac.pl
@@ -3,6 +3,9 @@
  * Lab 12 - Computing factorial
  */
 
-% Basic Usage: fac(+Number, -Factorial)
+% Usage: fac(Number, Factorial)
+
+% 0! = 1
 fac(0,1).
+% N! = N * (N-1)!
 fac(N,NF) :- N1 is N-1, fac(N1,N1F), NF is N1F * N.
