bdd63c12312740753324ce37ffca28beaf392df9
devansh-dvj <devansh.dvj@gmail.com>
2021-06-09 17:34:01 +0530

    Lab 12: Added comments to app

--- Lab 12/Submission/app.pl
+++ Lab 12/Submission/app.pl
@@ -3,6 +3,10 @@
  * Lab 12 - Appending a list to another list
  */
 
-% Basic Usage: app(+List1, +List2, -CombinedList)
+% Usage: app(List1, List2, CombinedList)
+
+% If List1 is empty then just List2
 app([],Y,Y).
+% Head of List1 is Head of Combined List and
+%   the rest is appending tail of List1 with List2
 app([H|X],Y,[H|Z]) :- app(X,Y,Z).
