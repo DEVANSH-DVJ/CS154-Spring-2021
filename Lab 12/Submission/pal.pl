f7d0efd4d73082db2e958ce8fa34c89f05ebabfb
devansh-dvj <devansh.dvj@gmail.com>
2021-06-09 17:34:20 +0530

    Lab 12: Added comments to pal

--- Lab 12/Submission/pal.pl
+++ Lab 12/Submission/pal.pl
@@ -3,7 +3,13 @@
  * Lab 12 - Check if a string is a palindrome
  */
 
-% Basic Usage: pal(+List)
+% Usage: pal(List)
+
+% Empty list is a palindrome
 pal([]).
+% Singleton list is a palindrome
 pal([_]).
+% List is a palindrome if
+%   first and last elements are equal and
+%   the remaining portion is also a palindrome
 pal(P) :- append([H|T],[H],P), pal(T).
