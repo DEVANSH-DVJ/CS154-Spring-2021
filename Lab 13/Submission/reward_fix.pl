3aa1c7d6b8806cc5ae5e8cb0c2a2c1db1e7ecb7a
devansh-dvj <devansh.dvj@gmail.com>
2021-06-14 17:06:25 +0530

    Lab 13: Added reward fixed

---
+++ Lab 13/Submission/reward_fix.pl
@@ -0,0 +1,12 @@
+btech(tikku).
+btech(ronu).
+btech(pinki).
+chessplayer(dillis).
+chessplayer(pinki).
+footballplayer(ronu).
+athelete(pinki).
+
+sportsperson(X) :- chessplayer(X), !.
+sportsperson(X) :- footballplayer(X), !.
+sportsperson(X) :- athelete(X), !.
+gatesreward(X) :- btech(X), sportsperson(X).
