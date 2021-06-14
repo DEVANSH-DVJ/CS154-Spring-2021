c4f493c71daa4f721c27d34a73ee5f7c3964a635
devansh-dvj <devansh.dvj@gmail.com>
2021-06-14 17:31:04 +0530

    Lab 13: Fixed typo in kth

--- Lab 13/Submission/kth.pl
+++ Lab 13/Submission/kth.pl
@@ -1,3 +1,3 @@
-kthfind([A|L],K,A,I) : -K is I.
-kthfind([A|L],K,E,I) :- kthfind(L,K,E,J), J is I+1.
+kthfind([A|_],K,A,I) :- K is I.
+kthfind([_|L],K,E,I) :- kthfind(L,K,E,J), J is I+1.
 kth([A|L],K,E) :- kthfind([A|L],K,E,0).
